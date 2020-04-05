bda8ef34b8b26dfaf08e197e533fc6d2a414f09a
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-04-05 10:47:14 +0530

    Added: Banking Cash Counter Simulator

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingCashCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Cash Counter");
+        }
+    }
+}
