b59f716237b2dc726de1900ad00e722be4a6731b
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-04-05 12:27:47 +0530

    Refactored: Customer class created for Encapsulate data

---
+++ Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Customer
+{
+	private string name;
+	private int accountNumber;
+	private int balance=500;
+	
+	public string Name { get => name; set => name = value; }
+	public int AccountNumber { get => accountNumber; set => accountNumber = value; }
+	public int Balance { get => balance; set => balance = value; }
+}
--- Program.cs
+++ Program.cs
@@ -4,9 +4,113 @@ namespace BankingCashCounter
 {
     class Program
     {
+        //Constants
+        private const int DEPOSIT = 0;
+        private const int WITHDRAW = 1;
+        private const int EXIT = 9;
+        private const int MINIMUM_BALANCE = 500;
+
+        //Variables
+        private static int choice = 0;
+        private static int amount = 0;
+
+        /// <summary>
+        /// Function to perform the transactions.
+        /// </summary>
+        static void DoTransaction()
+        {
+            Program program = new Program();
+            Customer customer = new Customer();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Enter 0 to Deposite");
+                    Console.WriteLine("Enter 1 to Withdraw");
+                    Console.WriteLine("Enter 9 to Exit");
+                    choice = Int32.Parse(Console.ReadLine());
+                }
+                catch (System.FormatException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                if (choice == EXIT)
+                {
+                    break;
+                }
+                switch (choice)
+                {
+                    case DEPOSIT:
+                        program.Deposit(customer);
+                        break;
+                    case WITHDRAW:
+                        program.Withdraw(customer);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Choice ");
+                        break;
+                }
+                
+            }
+        }
+
+        /// <summary>
+        /// Function for Deposit money to account.
+        /// </summary>
+        /// <param name="customer"></param>
+        public void Deposit(Customer customer)
+        {
+            try
+            {
+                Console.WriteLine("Enter Account Number");
+                customer.AccountNumber = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Amount");
+                amount = Int32.Parse(Console.ReadLine());
+                customer.Balance = customer.Balance + amount;
+                Console.WriteLine($"Updated Account Balance is {customer.Balance}");
+            }
+            catch (System.FormatException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+
+        /// <summary>
+        /// Function for Withdraw money from account.
+        /// </summary>
+        /// <param name="customer"></param>
+        public void Withdraw(Customer customer)
+        {
+            try
+            {
+                Console.WriteLine("Enter Account Number");
+                customer.AccountNumber = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Amount");
+                amount = Int32.Parse(Console.ReadLine());
+                if (customer.Balance - amount > MINIMUM_BALANCE)
+                {
+                    customer.Balance = customer.Balance - amount;
+                    Console.WriteLine($"Updated Account Balance is {customer.Balance}");
+                }
+                else
+                {
+                    Console.WriteLine("Account Balance Is Low");
+                }
+            }
+            catch (System.FormatException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+        }
+        
+        /// <summary>
+        /// Main Function for performing transaction.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Cash Counter");
+            Program.DoTransaction();
         }
     }
 }
