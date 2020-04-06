6d349ee53095535dba17764b350a1d6c04007825
shubhamdeulkar27 <shubhamdeulkar27@gmail.com>
2020-04-06 10:23:03 +0530

    Refcatored: Commenting Updates

--- Node.cs
+++ Node.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 
 /// <summary>
-/// Node for Linked List
+/// Node for Linked List.
 /// </summary>
 public class Node
-{
+{	//Variables for storing next node reference and data.
 	Customer data;
 	Node next;
 
+	//Setters and Getters for next and data variables
 	public Customer Data { get => data; set => data = value; }
 	public Node Next { get => next; set => next = value; }
 }
--- QNode.cs
+++ QNode.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 
 /// <summary>
-/// Summary description for Class1
+/// QNode Class Used as Node For Implemeting Queue
 /// </summary>
 public class QNode
-{
+{	//Variables for netx node reference and data.
 	int data;
 	QNode next;
 
+	//Setters and Getters for the next and Data Variables
 	public int Data { get => data; set => data = value; }
 	public QNode Next { get => next; set => next = value; }
 }
